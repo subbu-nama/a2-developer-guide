e0446aba694e6bc77c1e350ba5827d63a8ddda41
Subbu Nama <subbu.nama@walkingtree.in>
2017-02-23 20:09:19 +0530

    Pushing changes with EE

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -6,9 +6,27 @@ describe('a2componentsdoc App', function() {
   beforeEach(() => {
     page = new A2componentsdocPage();
   });
-
-  it('should display message saying app works', () => {
+ 
+  // Ensure Header area content
+  it('check header area', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getHeaderText()).toEqual('HEADER AREA');
   });
+
+  // Ensure left side area content
+  it('check left side area', () => {
+    page.navigateTo();
+    expect(page.getLeftAreaText()).toEqual('LEFT SIDE AREA');
+  });
+
+ // Ensure center area has records or not
+  it('check right side area', () => {
+    page.navigateTo();
+    page.getCenterAreaText()
+    .then((value:any) => {
+                  expect(value).toEqual('1');
+                }
+            );
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -4,8 +4,19 @@ export class A2componentsdocPage {
   navigateTo() {
     return browser.get('/');
   }
+  
+  // Get Header area content
+  getHeaderText() {
+    return element(by.css('app-root .layout-header-area-alignment h1')).getText();
+  }
+  
+  // Get Left side area content
+  getLeftAreaText() {
+    return element(by.css('app-root .layout-left-area-alignment h1')).getText();
+  }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  // Ensure Center area records
+  getCenterAreaText() {
+    return element(by.css('app-root .layout-center-area-alignment table tbody tr td')).getText();
   }
 }
